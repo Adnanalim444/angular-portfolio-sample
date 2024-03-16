efb97635f197c55824d59c3950075d21a6d225f0
adnanalim444 <adnanalim444@gmail.com>
2024-03-15 19:34:08 -0700

    set conponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./hero/hero.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { WorkExperienceComponent } from "./work-experience/work-experience.component";
+import { ProjectComponent } from "./projects/project.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeroComponent, SkillsComponent, WorkExperienceComponent, ProjectComponent, ContactComponent]
 })
 export class AppComponent {
   title = 'portfolio';
